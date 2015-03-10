1abad7fb30f77e1596bc8a1c0e770e2dc4399c9f
axton21 <axton_n@yahoo.com>
2015-03-10 10:31:34 -0400

    add my_count function

--- lib/string_theory.rb
+++ lib/string_theory.rb
@@ -1,8 +1,21 @@
 require 'pry'
+
   def sort_by_appearance(characters_to_sort, long_text)
     array = []
     char_array = characters_to_sort.split("")
-    result = char_array.each_with_object({}){|char, x| x[long_text.count(char)] = char}
+    result = char_array.each_with_object({}){|char, x| x[long_text.my_count(char)] = char}
     keys = result.keys.sort.reverse
     keys.map{|x| result[x]}.join
   end
+
+class String
+  def my_count(char)
+    array = []
+    self.split("").each do |x|
+      if x == char
+        array << x
+      end
+    end
+    return array.size
+  end
+end
