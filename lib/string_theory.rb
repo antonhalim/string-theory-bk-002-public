bc40d07db498c3e158c356ab4492db1b911d7175
axton21 <axton_n@yahoo.com>
2015-03-10 09:58:03 -0400

    Labs are like a box of chocolates

--- lib/string_theory.rb
+++ lib/string_theory.rb
@@ -1,3 +1,8 @@
-def sort_by_appearance(characters_to_sort, long_text)
-  # code goes here
-end
+require 'pry'
+  def sort_by_appearance(characters_to_sort, long_text)
+    array = []
+    char_array = characters_to_sort.split("")
+    result = char_array.each_with_object({}){|char, x| x[long_text.count(char)] = char}
+    keys = result.keys.sort.reverse
+    keys.map{|x| result[x]}.join
+  end
